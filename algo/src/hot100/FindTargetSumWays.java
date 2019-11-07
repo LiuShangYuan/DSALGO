b02d01d0a1df155b148a9d85ab50934f64f0b921
yinkai <2961709898@qq.com>
2019-11-07 21:45:16 +0800

    目标和

--- algo/src/hot100/FindTargetSumWays.java
+++ algo/src/hot100/FindTargetSumWays.java
@@ -1,5 +1,7 @@
 package hot100;
 
+import java.util.Arrays;
+
 /**
  * 494 目标和
  */
@@ -7,7 +9,7 @@ public class FindTargetSumWays {
     public static void main(String[] args) {
         int[] nums = {1, 1, 1, 1, 1};
 //        int[] nums = {};
-        System.out.println(new FindTargetSumWays().findTargetSumWays(nums, 3));
+        System.out.println(new FindTargetSumWays().findTargetSumWays_dp(nums, 3));
     }
 
     public int findTargetSumWays(int[] nums, int S) {
@@ -26,6 +28,39 @@ public class FindTargetSumWays {
         }
 
         return help(nums, S - nums[start], start + 1) + help(nums, S + nums[start], start + 1);
+
+    }
+
+
+    /**
+     * 转化成0-1背包问题
+     *
+     * @param nums
+     * @param S
+     */
+    public int findTargetSumWays_dp(int[] nums, int S) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+
+
+        int[] dp = new int[(sum + S) / 2 + 1];
+        dp[0] = 1;
+
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = (sum + S) / 2; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+
+        return dp[(sum + S) / 2];
 
     }
 }
